6e3da000bea81f118632b320354ecfcc825dee92
ARIYANEM <emami8637@gmail.com>
2022-09-26 12:45:58 +0330

    add decorator

---
+++ python/decorator.py
@@ -0,0 +1,12 @@
+def announce(hello):
+    def wrapper():
+        print("about to run decorator funcation")
+        hello()
+        print("i can add down function in this function")
+    return wrapper
+
+@announce
+def h ():
+    print("hello go on function wrapper")
+
+h()
